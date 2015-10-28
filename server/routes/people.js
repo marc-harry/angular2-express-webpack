1a4cc93252918944185abefbf27b9edb487a8d73
Marc Harry <mjharry1@gmail.com>
2015-10-28 14:16:03 +0000

    Removed built js files

--- server/routes/people.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var people_controller_1 = require('../controllers/people.controller');
-router.get('/people', people_controller_1.default.index);
-module.exports = router;
--- server/routes/todos.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var todos_controller_1 = require('../controllers/todos.controller');
-router.get('/todos', todos_controller_1.default.index);
-module.exports = router;
