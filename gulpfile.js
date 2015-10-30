04b9832c67e03c03bb2754e154ffd8a12d1d388c
Marc Harry <mjharry1@gmail.com>
2015-10-30 16:12:35 +0000

    Added gulp task to compile angular ts

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,34 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     nodemon = require('gulp-nodemon');
 
+var tsconfig = {
+    target: 'ES5',
+    module: 'commonjs',
+    declaration: false,
+    noImplicitAny: false,
+    removeComments: true,
+    noLib: false,
+    emitDecoratorMetadata: true,
+    experimentalDecorators: true
+};
+
 gulp.task('compile-ts', function () {
     return gulp.src(['server/**/*.ts', 'typings/**/*.ts'])
         .pipe(sourcemaps.init())
-        .pipe(tsc({
-            target: 'ES5',
-            module: 'commonjs',
-            declaration: false,
-            noImplicitAny: false,
-            removeComments: true,
-            noLib: false,
-            emitDecoratorMetadata: true,
-            experimentalDecorators: true
-        }))
+        .pipe(tsc(tsconfig))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('server'));
 });
 
-gulp.task('start', ['compile-ts'], function () {
+gulp.task('compile-angular-ts', function () {
+    return gulp.src(['src/app/**/*.ts', 'typings/**/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tsconfig))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('src/app'));
+});
+
+gulp.task('start', ['compile-ts', 'compile-angular-ts'], function () {
    nodemon({
         script: 'server/server.js',
         ext: 'ts',
